677f9e2e631703c11afe0f0389cdb27590bb7e6f
João Pedro Bergfeld Backer Lopes <113688538+jpedrobacker@users.noreply.github.com>
2023-10-27 18:32:02 +0000

    minor fix

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,19 +12,17 @@
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(char *s)
+size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
 	i = 0;
-	if (!s)
-		return (0);
 	while (s[i] != '\0')
 		i++;
 	return (i);
 }
 
-char	ft_strchr(const char *str, int c)
+char	*ft_strchr(const char *str, int c)
 {
 	int		i;
 	char	*stri;
--- maintest.c
+++ maintest.c
@@ -10,12 +10,8 @@ int main(void)
 	path = "text.txt";
 	fd = open(path, O_RDONLY);
 	i = 0;
-	while (i < 6)
-	{
-		str = get_next_line(fd);
-		printf("fd: %i, %s\n", fd, str);
-		i++;
-	}
+	str = get_next_line(fd);
+	printf("fd: %i, %s\n", fd, str);
 	close(fd);
 	return (0);
 }
